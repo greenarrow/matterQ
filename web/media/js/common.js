974b056a2905ee3baf0a24506d42a113a22ce85c
Stefan Blanke <greenarrow@users.sourceforge.net>
2013-03-21 23:40:52 +0000

    Simple javascript to load AJAX backend

--- web/media/js/common.js
+++ web/media/js/common.js
@@ -1,4 +1,26 @@
-function auto_refresh(period) {
-    setTimeout("location.reload(true);", period);
-}
+$(document).ready(function() {
+    function bind() {
+        $("a.detail").click(function() {
+            $.get($(this).attr("href"), function(data) {
+                alert(data);
+            });
+            return false;
+        });
 
+        $("a.cancel").click(function() {
+            $.get($(this).attr("href"), function(data) {
+                alert(data);
+                reload();
+            });
+            return false;
+        });
+    }
+
+    function reload() {
+        $("#status").load("/ajax/lp/status", bind);
+        $("#queue").load("/ajax/lp/queue", bind);
+    }
+
+    reload();
+    window.setInterval(reload, 5000);
+});
