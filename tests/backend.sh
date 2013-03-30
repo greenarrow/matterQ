74685a3e17498484391975ce714f52e9ee04b6d1
Stefan Blanke <greenarrow@users.sourceforge.net>
2013-03-30 11:07:21 +0000

    Add lprng backend test
    Allows testing of the whole planner system without a real lprng server
    running. Arguments are the sequence of gcode files to print.

---
+++ tests/backend.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+set -e
+
+source ../config/matterq.conf
+
+
+PATH=../scripts:$PATH:.
+MQ_TESTMODE=1
+MQ_SPOOLDIR=/tmp/spoolmq
+#SPOOL_DIR=/tmp/spoollpr
+SPOOL_DIR=""
+
+TEST_OUTPUT=/tmp/mqtest
+
+export PATH
+export MQ_TESTMODE
+export MQ_SPOOLDIR
+export MQ_HEADSIZE
+export MQ_PRINTBED
+export SPOOL_DIR
+
+#mkdir -p $SPOOL_DIR
+mkdir -p $MQ_SPOOLDIR/depositions
+mkdir -p $MQ_SPOOLDIR/images
+mkdir -p $TEST_OUTPUT
+
+rm -f $TEST_OUTPUT/*
+rm -f $MQ_SPOOLDIR/depositions/*
+rm -f $MQ_SPOOLDIR/images/*
+
+declare -i TALLY=0
+
+
+while [ $# -ne 0 ]
+do
+    DATAFILES=$1
+    export DATAFILES
+    echo "JOB START: $DATAFILES"
+
+    set +e
+    matterq-lprng
+    RC=$?
+    set -e
+
+    echo "JOB EXIT $RC"
+
+    mv $MQ_SPOOLDIR/images/current.svg $TEST_OUTPUT/out-$TALLY.svg
+
+    if [ "$RC" -eq 2 ]
+    then
+        echo "abort"
+        exit 1
+    fi
+
+    let TALLY=$TALLY+1
+    shift
+done
+
+
